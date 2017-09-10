54fdf6cbcf209525675c84c06b0542ea5540c406
atelierbram <bramdeh@gmail.com>
2017-09-10 13:05:51 +0200

    added download button for vector illustration in demo

--- src/lake/index.php
+++ src/lake/index.php
@@ -16,10 +16,15 @@ include '../inc/header.inc';
   include '../inc/demo-tiles-header.inc';
   include '../inc/demo-tiles.inc';
   include '../inc/code-samples.inc';
-  include '../../output/svg/interior-base2tone-lake.svg.inc';
-  // include '../../output/svg/circles-base2tone-lake.svg.inc';
-  include '../../output/html/table/table-base2tone-lake.inc';
-?>
+ob_start('compress_markup'); ?>
+  <section class="svg-interior">
+    <h3 class="h3 _mbxl">Templates for illustrations</h3>
+    <div class="mw-75vh">
+    <?php include '../../output/svg/interior-base2tone-lake.svg.inc'; ?>
+  <div class="cf"><a href="../../output/svg/interior-base2tone-<?= $theme ?>.svg.inc" class="button _mtm">download vector image</a><p class="credits small _mtm"><a href="//www.freepik.com/free-vector/house-interior-with-armchair-and-shelves_1056598.htm">Designed by Freepik</a></p>
+</div><!-- /.cf --></div><!-- /.mw-75vh -->
+</section><?php ob_end_flush(); function compress_markup($buffer) { $search = array("/>[[:space:]]+/", "/[[:space:]]+</"); $replace = array(">","<"); return preg_replace($search, $replace, $buffer); } ?>
+    <?php include '../../output/html/table/table-base2tone-lake.inc'; ?>
   </div> <!-- /.content #content -->
 </div><!-- /.container-flex #main -->
 <?php include '../inc/footer.inc'; ?>
